1b61fb9b4849eba041abe1f992924305df11ae58
seonrizee <seonrizee@gmail.com>
2025-02-05 21:31:35 +0900

    feat: Step 2: React로 정적인 버전 구현하기

--- Quick Start/Thinking in React/src/App.jsx
+++ Quick Start/Thinking in React/src/App.jsx
@@ -1,10 +1,96 @@
-function App() {
-
+function ProductCategoryRow({category}) {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
   )
 }
 
-export default App
+function ProductRow({product}) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+  return (
+      <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  )
+}
+
+function ProductTable({products}) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach(product => {
+    if (product.category !== lastCategory) {
+      rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category}
+          />
+      );
+    }
+    rows.push(
+        <ProductRow
+          product={product}
+          key={product.name}
+        />
+    );
+    lastCategory = product.category;
+  })
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  )
+}
+
+function SearchBar() {
+  return(
+      <form>
+        <input type="text" placeholder="Search" />
+        <label>
+          <input type="checkbox" />
+          {SEARCHBAR_CHECKBOX_LABEL}
+        </label>
+      </form>
+  )
+}
+
+function FilterableProductTable({products}) {
+
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  )
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+const SEARCHBAR_CHECKBOX_LABEL = ' Only show products in stock';
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
+};
+
